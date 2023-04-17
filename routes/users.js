b5040065ca5984c55991372d8273c1bf0a152116
SangeethaShanmugm <MY_NAME@example.com>
2023-04-17 13:43:00 +0530

    updtaed

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { genPassword, createUser } from "../helper.js";
+import { genPassword, createUser, getUserByName } from "../helper.js";
 
 const router = express.Router();
 
@@ -7,6 +7,19 @@ const router = express.Router();
 router.post("/signup", async (req, res) => {
   const { username, password } = req.body;
   console.log(username, password);
+  //validation
+  const isUserExist = await getUserByName(username);
+  console.log(isUserExist);
+  if (isUserExist) {
+    res.status(400).send({ message: "Username already Taken" });
+    return;
+  }
+  if (
+    !/^(?=.*?[0-9])(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[#!@%&_]).{8,}$/g.test(password)
+  ) {
+    res.status(400).send({ message: "Password Pattern doesnt match" });
+    return;
+  }
   const hashedPassword = await genPassword(password);
   const result = await createUser(username, hashedPassword);
   res.send(result);
